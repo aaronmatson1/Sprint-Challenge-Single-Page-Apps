88f72cbd1a6b328e40012d31f2850d5f6cca03aa
aaronmatson1 <matsonaaron1@gmail.com>
2020-02-23 21:28:08 -0700

    API Timed out and cannot fine tune. MVP Met, though

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,4 +1,47 @@
 import React from "react";
+import styled from "styled-components";
+
+const Card = styled.div `
+  display:flex;
+  flex-direction:row;
+  justify-content:center;
+  margin:25px;
+  text-align:center;
+  border: 1px solid #39ff14;
+`
+const Info = styled.div`
+  // border: 1px solid blue;
+  width: auto;
+  padding: 5px;
+`
+const Header = styled.h1`
+  color: #000099
+  text-align: center;
+`
+const Header3 = styled.h3`
+  color: #3333ff
+`
+export default function CharacterCard(props) {
+  // return <span>todo: character</span>;
+
+  console.log(props);
+
+
+  return (
+
+    <Card>
+      {/* <img alt="character" src={props.character.image} /><br/> */}
+
+        <div>
+      <img alt="character" src={props.character.image} /></div>
+      <Info>
+      <Header>{props.character.name}</Header><br/>
+      <Header3>{props.character.species}</Header3><br/>
+      <Header3>{props.character.gender}</Header3>
+      </Info>
+
+    </Card>
+  );
 
 export default function CharacterCard() {
   return <span>todo: character</span>;
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,37 @@
 import React, { useEffect, useState } from "react";
+import axios from axios;
+import CharacterCard from "./CharacterCard";
+import styled from "styled-components";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+    const [character, setCharacter] = useState([]);
 
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    useEffect(() => {
+        // TODO: Add API Request here - must run in `useEffect`
+        //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
 
-  return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
-  );
-}
+        //adding API requests
+        axios
+        .get('https://rick-api.herokuapp.com/api/')
+
+        .then(response => {
+          setCharacter(response.data.results)
+          //Checking console
+          console.log(response.data.results)
+        })
+        .catch(error => {
+          //write to console that there was an error;
+          console.log('Server Error', error)
+        })
+
+      }, []);
+
+      return (
+        <div className="CharacterCard">
+        {character.map(character => (
+          <CharacterCard key={character.id} character={character} />
+        ))}
+        </div>
+      );
+    }
